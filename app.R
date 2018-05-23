3b7f58b967693da34a5dd68d99e4140a6fc32caa
Jeremy Chang <jeremyhchang@gmail.com>
2018-05-23 16:51:50 -0700

    load webAPI

--- app.R
+++ app.R
@@ -5,24 +5,27 @@
 ## Rey Matsunaga
 ## Israel Sixto-Sanchez
 
+library("dplyr")
+library("ggplot2")
+library("tidyverse")
+library("shiny")
+library("httr")
+library("jsonlite")
+
+source("api_token.R")
 #####################
 ######## API ########
 #####################
-request_topic <- function(topic) {
-  resource <- paste0("https://data.seattle.gov/resource/y7pv-r3kh.json")
-  response <- GET(paste0(base_uri, bill_resource), add_headers("X-API-Key" = api_key))
-  fromJSON <- fromJSON(content(bill_response, "text"))
-  data <- flatten(data.frame(bill_fromJSON$results$bills))
+resource <- paste0("https://data.seattle.gov/resource/y7pv-r3kh.json")
+response <- GET(resource, add_headers(token))
+fromJSON <- fromJSON(content(response, "text"))
+data <- flatten(data.frame(fromJSON))
   
-  bill_data <- bill_data[1:10, ]
-  legislator <- paste(bill_data$sponsor_name, bill_data$sponsor_party, bill_data$sponsor_state, sep = ", ")
-  short_df <- data.frame(bill_data$bill_id, bill_data$short_title, 
-                         legislator, bill_data$active, 
-                         bill_data$govtrack_url)
-  data_columns <- c("Bill ID", "Name", "Legislator", "Active", "URL")
-  colnames(short_df) <- data_columns
-  short_df
-}
+View(data)
+
+data_columns <- c("Bill ID", "Name", "Legislator", "Active", "URL")
+colnames(short_df) <- data_columns
+short_df
 
 #####################
 #### SECTION ONE ####
