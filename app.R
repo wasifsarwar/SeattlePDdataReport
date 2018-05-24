cc996e86fa05d9609a2e70b7edbd50e58bb45c38
wasifsarwar <wasif.sarwar9@gmail.com>
2018-05-23 17:01:56 -0700

    fix merge

--- app.R
+++ app.R
@@ -5,42 +5,28 @@
 ## Rey Matsunaga
 ## Israel Sixto-Sanchez
 
-<<<<<<< HEAD
-library("dplyr")
-library("ggplot2")
-library("tidyverse")
-library("shiny")
-library("httr")
-library("jsonlite")
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(shiny)
+library(httr)
+library(jsonlite)
 
 source("api_token.R")
+
 #####################
 ######## API ########
 #####################
+
 resource <- paste0("https://data.seattle.gov/resource/y7pv-r3kh.json")
 response <- GET(resource, add_headers(token))
 fromJSON <- fromJSON(content(response, "text"))
 data <- flatten(data.frame(fromJSON))
   
-View(data)
-
-data_columns <- c("Bill ID", "Name", "Legislator", "Active", "URL")
-colnames(short_df) <- data_columns
-short_df
-=======
-library(httr)
-library(jsonlite)
-
 #####################
 ######## API ########
 #####################
 
-resource <- paste0("https://data.seattle.gov/resource/y7pv-r3kh.json")
-response <- GET(resource, add_headers("X-API-Key" = token))
-fromJSON <- fromJSON(content(response, "text"))
-data <- flatten(data.frame(fromJSON$results$bills))
-
->>>>>>> wasif
 
 #####################
 #### SECTION ONE ####
