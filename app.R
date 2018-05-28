2113863af50302885d2c78b00c427336833d42b8
wasifsarwar <wasif.sarwar9@gmail.com>
2018-05-27 22:31:43 -0700

    ui and server setup

--- app.R
+++ app.R
@@ -11,6 +11,7 @@ library(tidyverse)
 library(shiny)
 library(httr)
 library(jsonlite)
+library(leaflet)
 
 #####################
 ######## API ########
@@ -18,48 +19,111 @@ library(jsonlite)
 
 
 data <- read.csv("./data/Seattle_police_data_2017.csv",na.strings = "NA",
-                   stringsAsFactors = FALSE,fill = TRUE, header = TRUE)
+                   stringsAsFactors = FALSE, fill = TRUE, header = TRUE)
+offense_type <- unique(data$Offense.Type)
+#colnames(offense_type) <- 
 
 ## we're only doing records for 2017
 
  
 ui <- fluidPage(
-  titlePanel("Eviction Data Report in the United States"),
-  sidebarLayout(
-    sidebarPanel(
-      # State Widget
-      selectInput("State", "Select State", choices = state_data$name, selected = ""),
-      # Year Slider
-      sliderInput('year_choice', label = "Choose Year", min = year_range[1], 
-                  max = year_range[2], value = 2000,step = 1)
+  titlePanel("Seattle PD data report 2017"),
+  
+  tabsetPanel(
+    tabPanel("HeatMap", fluid = TRUE,
+      sidebarLayout(
+        sidebarPanel(
+        # State Widget
+          selectInput("crime_type", "Offense Type", choices = offense_type, selected = "")
+      ),
+        mainPanel(
+          leafletOutput("heatmap"),
+          p()
+        )
+      )
     ),
-    mainPanel(
-      tabsetPanel( 
-        tabPanel("Table", br(), strong(textOutput("table_info")), br(), tableOutput("table")),
-        tabPanel("Plot", br(), strong(textOutput("plot_info")), br(), plotOutput("plot"
-                                                                                 , click = "plot_click"), em(textOutput("click_info"))) 
+    tabPanel("Jeremy", fluid = TRUE,
+      sidebarLayout(   # layout the page in two columns
+        sidebarPanel(  # specify content for the "sidebar" column
+          p("sidebar panel content goes here")
+        ),
+        mainPanel(     # specify content for the "main" column
+          p("main panel content goes here")
+        )
+      )
+    ),
+    tabPanel("Rey" , fluid = TRUE,
+      sidebarLayout(   # layout the page in two columns
+        sidebarPanel(  # specify content for the "sidebar" column
+          p("sidebar panel content goes here")
+        ),
+        mainPanel(     # specify content for the "main" column
+          p("main panel content goes here")
+        )
+      )
+    ),
+    tabPanel("Israel", fluid = TRUE,
+      sidebarLayout(   # layout the page in two columns
+        sidebarPanel(  # specify content for the "sidebar" column
+          p("sidebar panel content goes here")
+        ),
+        mainPanel(     # specify content for the "main" column
+          p("main panel content goes here")
+        )
       )
     )
   )
 )
-#####################
-#### SECTION ONE ####
-#####################
-#Wasif: map visualization for a heat map, categorized by type of crime. Analysis: talk about the heat map
+
+server <- function(input,output) {
+  
+  
+  # reactive variable for shared data
+  filtered_table <- reactive({
+    crime_data <- data %>%
+      filter(Offense.type == input$crime_type[1]) 
+    
+    result <- select(crime_data,Date.Reported,Longitude,Latitude,Location,
+                     Month,Year)
+    return(result)
+  })
+  
+  output$heatmap <- renderLeaflet({
+    data_plot <- 
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.TonerLite,
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>%
+      addMarkers(data = points())
+  })
+  
+  #####################
+  #### SECTION ONE ####
+  #####################
+  #Wasif: map visualization for a heat map, categorized by type of crime. Analysis: talk about the heat map
+  
+  
+  #####################
+  #### SECTION TWO ####
+  #####################
+  
+  
+  #######################
+  #### SECTION THREE ####
+  #######################
+  
+  
+  
+  ######################
+  #### SECTION FOUR ####
+  ######################
+  
+}
+  
+# Create a new `shinyApp()` using the above ui and server
+shinyApp(ui = ui, server = server)
 
 
-#####################
-#### SECTION TWO ####
-#####################
 
 
 
-#######################
-#### SECTION THREE ####
-#######################
-
-
-
-######################
-#### SECTION FOUR ####
-######################
