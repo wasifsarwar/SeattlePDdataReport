e892fce2df82a91184a3e6bae882034f4376cd1b
wasifsarwar <wasif.sarwar9@gmail.com>
2018-05-27 19:34:04 -0700

    add data file

--- app.R
+++ app.R
@@ -19,6 +19,16 @@ library(jsonlite)
 
 data <- read.csv("./data/Seattle_Police_Department_Police_Report_Incident.csv",na.strings = "NA",
                    stringsAsFactors = FALSE,fill = TRUE, header = TRUE)
+
+## we're only doing records for 2017
+
+data <- data %>% 
+        filter(Year == 2017) %>% 
+        select(Offense.Type,Summarized.Offense.Description,Date.Reported,
+               Occurred.Date.or.Date.Range.Start,Occurred.Date.Range.End,
+               Hundred.Block.Location,District.Sector,Longitude,Latitude,
+               Location,Month,Year)
+
  
 
 #####################
