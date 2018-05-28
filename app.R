9d0e935b6ed172673a034430259bdb7a343be468
wasifsarwar <wasif.sarwar9@gmail.com>
2018-05-27 19:44:18 -0700

    ui layout

--- app.R
+++ app.R
@@ -17,24 +17,35 @@ library(jsonlite)
 #####################
 
 
-data <- read.csv("./data/Seattle_Police_Department_Police_Report_Incident.csv",na.strings = "NA",
+data <- read.csv("./data/Seattle_police_data_2017.csv",na.strings = "NA",
                    stringsAsFactors = FALSE,fill = TRUE, header = TRUE)
 
 ## we're only doing records for 2017
 
-data <- data %>% 
-        filter(Year == 2017) %>% 
-        select(Offense.Type,Summarized.Offense.Description,Date.Reported,
-               Occurred.Date.or.Date.Range.Start,Occurred.Date.Range.End,
-               Hundred.Block.Location,District.Sector,Longitude,Latitude,
-               Location,Month,Year)
-
  
-
+ui <- fluidPage(
+  titlePanel("Eviction Data Report in the United States"),
+  sidebarLayout(
+    sidebarPanel(
+      # State Widget
+      selectInput("State", "Select State", choices = state_data$name, selected = ""),
+      # Year Slider
+      sliderInput('year_choice', label = "Choose Year", min = year_range[1], 
+                  max = year_range[2], value = 2000,step = 1)
+    ),
+    mainPanel(
+      tabsetPanel( 
+        tabPanel("Table", br(), strong(textOutput("table_info")), br(), tableOutput("table")),
+        tabPanel("Plot", br(), strong(textOutput("plot_info")), br(), plotOutput("plot"
+                                                                                 , click = "plot_click"), em(textOutput("click_info"))) 
+      )
+    )
+  )
+)
 #####################
 #### SECTION ONE ####
 #####################
-
+#Wasif: map visualization for a heat map, categorized by type of crime. Analysis: talk about the heat map
 
 
 #####################
